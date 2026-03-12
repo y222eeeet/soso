33ba580b40d95f3a117c150dd0695c939d9cac9c
Jiwoo Seo <ji2003woo@sookmyung.ac.kr>
2026-03-12 17:04:46 +0900

    Initial Commit

---
+++ soso/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  soso
+//
+//  Created by 서지우 on 3/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ soso/sosoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sosoApp.swift
+//  soso
+//
+//  Created by 서지우 on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct sosoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
